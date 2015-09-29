c109e13c1c052898a1bc740ce177c0d5a8007eeb
AgusDOLARD <agust_22_10@hotmail.com>
2015-09-29 13:36:10 -0300

    Actualizacion Base de Datos

--- Project/app/src/main/java/com/dataBase/BaseDeDatos.java
+++ Project/app/src/main/java/com/dataBase/BaseDeDatos.java
@@ -18,15 +18,75 @@ public class BaseDeDatos extends SQLiteOpenHelper{
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("CREATE TABLE IF NOT EXIST Bebes(id INT primary key  NOT NULL, name TEXT, birth DATETIME, height INT, weight INT)");
-        db.execSQL("CREATE TABLE IF NOT EXIST Actividades(id INT primary key NOT NULL, nameAct TEXT, date DATETIME, idBebe INT)");
+        db.execSQL("CREATE TABLE IF NOT EXISTS Bebes(id INTEGER PRIMARY KEY AUTOINCREMENT, name Text,date Text, height INTEGER, weight INTEGER)");
+        db.execSQL("CREATE TABLE IF NOT EXISTS Actividades(id INTEGER PRIMARY KEY AUTOINCREMENT, nameAct Text, date Text, idBebe INTEGER)");
+
+    }
+    /* public void create(View view){
+    *  try{
+            BaseDeDatos dab = new BaseDeDatos(this);
+            SQLiteDatabase db = dab.getWritableDatabase();
+            ContentValues rg =new ContentValues();
+            //rg.put("id" , 1);
+            rg.put("name", "Bringas");
+            rg.put("height", 3);
+            rg.put("weight", 40);
+            rg.put("date", "8:09 AM");
+            long i = db.insert("Bebes", null, rg);
+            db.close();
+
+
+            if (i>0){
+                    Toast.makeText(this,"Creado", LENGTH_SHORT).show();
+                }
+
+        }
+        catch(Exception e){
+            System.out.println(e);
+            Toast.makeText(this,"No Creado", LENGTH_SHORT).show();
+        }
+
+
+     public void delete(int id) {
+        BaseDeDatos admin = new BaseDeDatos(this);
+        SQLiteDatabase bd = admin.getWritableDatabase();
+        bd.delete("Bebes", "id=" + id, null);
+        bd.close();
     }
 
+
+        public void listar(){
+        try{BaseDeDatos dab = new BaseDeDatos(this);
+            SQLiteDatabase db = dab.getReadableDatabase();
+            Cursor fila = db.rawQuery("SELECT * from Bebes", null);
+            int cant = fila.getCount();
+            int i = 0;
+            String[] arr = new String[cant];
+            if (fila.moveToFirst()){
+                do {
+
+                    String linea = fila.getInt(0)+" "+fila.getString(1);
+                    arr [i] = linea;
+                    i++;
+                }while(fila.moveToNext());{
+
+                }
+                ArrayAdapter<String > adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, arr);
+                list.setAdapter(adapter);
+            }
+        }catch (Exception e ){
+            e.printStackTrace();
+        }
+    }
+
+
+    * */
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVesion) {
         //TODO Para actualizar Db
         //
     }
+
 
 
 }
